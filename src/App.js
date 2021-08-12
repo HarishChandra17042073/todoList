cd95f1bc9838fd4d59b0f56fef13cfbb00d92740
HarishChandra17042073 <hc82223@gmail.com>
2021-08-12 09:46:54 +0530

    this is a todo list

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import data from "./data.json";
+//components
+import Header from "./Header";
+import ToDoList from "./ToDoList";
+ 
+import './App.css';
+ 
+function App() {
+  const [ toDoList, setToDoList ] = useState(data);
+ 
+ 
+ return (
+   <div className="App">
+     <Header />
+     <ToDoList toDoList={toDoList}/>
+   </div>
+ );
+}
+const handleToggle = (id) => {
+  let mapped = toDoList.map(task => {
+    return task.id == id ? { ...task, complete: !task.complete } : { ...task};
+  });
+  setToDoList(mapped);
+}
+ 
+export default App;
